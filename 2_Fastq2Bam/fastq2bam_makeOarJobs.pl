5dec8fd27332cdd581135e6eecf0b80d9c4165ab
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2021-02-23 17:36:10 +0100

    fastq2bam_makeOarJobs.pl : adapt to new args of fastq2bam.pl, improve doc

--- 2_Fastq2Bam/fastq2bam_makeOarJobs.pl
+++ 2_Fastq2Bam/fastq2bam_makeOarJobs.pl
@@ -3,17 +3,18 @@
 # 19/06/2019
 # NTM
 #
-# submit fastq2bam jobs on dahu with oar.
-# I don't want to modify fastq2bam.pl (eg it's good that all the
-# sanity checks are performed on each node / by each job), so I
-# am just calling fastq2bam.pl many times on individual samples.
-
+# submit fastq2bam jobs on the dahu cluster with OAR.
+# Not sure if this script can be useful outside our organization:
+# it would need adapting for a different batch scheduler and/or
+# cluster, paths and filename patterns are hard-coded (eg samples
+# are called grexome\d\d\d\d), etc... 
 
 use strict;
 use warnings;
 
-# takes 2 args: $first and $last, the first and last grexomes
-# for this run of fastq2bam_makeOarJobs.pl
+# takes 2 args: $first and $last, these are ints and assumes that
+# all samples are named grexome\d\d\d\d , the 4-digit integer will 
+# range from $first to $last for this run of fastq2bam_makeOarJobs.pl.
 # NOTE: not more than 50 at a time, or you get error:
 # Admission Rule ERROR : [ADMISSION RULE] Error: you cannot have more than 50 jobs waiting in the queue at the same time.
 (@ARGV == 2) || die "E: need two ints as arguments, first and last\n";
@@ -39,9 +40,11 @@ my $logDir = "~/Fastq2Bam_Dahu_stdouterr/";
 # binaries are in $binDir
 my $binDir = "~/Fastq2Bam_PackagedWithBinaries_Centos7/";
 # fastqs are in $inDir
-my $inDir = "/bettik/nthierry/FASTQs_All_Grexomized/";
+my $inDir = "/bettik/thierryn/FASTQs_All_Grexomized/";
 # produce bams in $outDir
-my $outDir = "/bettik/nthierry/BAMs_grexome_NTM/BAMs_NTM_Dahu/";
+my $outDir = "/bettik/thierryn/BAMs_grexome_NTM/BAMs_NTM_Dahu/";
+# ref genome
+my $genome = "/bettik/thierryn/HumanGenome/hs38DH.fa";
 
 # oarsub command with params:
 # run on my project ngs-timc, ask for 16 cores on 1 node, 2h walltime max
@@ -61,7 +64,7 @@ foreach my $gNum ($first..$last) {
     $grexome = "grexome$grexome";
     # choose stdout and stderr filenames
     my $oar = $oarBase."-O $logDir/fastq2bam.$grexome.out -E $logDir/fastq2bam.$grexome.err ";
-    $oar .= "\"perl $binDir/fastq2bam.pl --out $outDir --in $inDir --first $gNum --last $gNum --bin $binDir --bwakit $binDir --threads $threads --real\"";
+    $oar .= "\"perl $binDir/fastq2bam.pl --out $outDir --in $inDir --samples $grexome --bin $binDir --bwakit $binDir --threads $threads --genome $genome -real\"";
 
     system($oar);
 

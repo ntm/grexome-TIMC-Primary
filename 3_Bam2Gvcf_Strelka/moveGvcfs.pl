5aaaac6cdf54eb379f52da846f68bb0f852a61f1
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2021-02-26 15:23:45 +0100

    3_Bam2Gvcf_Strelka/moveGvcfs.pl : no more hard-coding of samples as grexome*, better logging and documentation

--- 3_Bam2Gvcf_Strelka/moveGvcfs.pl
+++ 3_Bam2Gvcf_Strelka/moveGvcfs.pl
@@ -5,41 +5,50 @@
 # NTM
 
 # Takes 2 args: inDir, outDir.
-# grab the genome.S1.vcf.gz and associated .tbi files in each 
-# grexomeXXXX subdir of $inDir, and move them to $outDir, 
-# renaming to grexomeXXXX.g.vcf.gz .
+# inDir contains one or more subdirs whose names are sampleIDs, each
+# containing the results (files and subdirs) of a STRELKA run.
+# 
+# Grab the genome.S1.vcf.gz and associated .tbi files in each 
+# $sample subdir of $inDir, and move them to $outDir, 
+# renaming to $sample.g.vcf.gz .
 
 use strict;
 use warnings;
+use File::Basename qw(basename);
 
-# inDir: dir containing grexomeXXXX subdirs as produced by strelka
+# we use $0 in every stderr message but we really only want
+# the program name, not the path
+$0 = basename($0);
+
+
+# inDir: dir containing $sampleID subdirs as produced by strelka
 my $inDir;
 
 # outDir: dir where GVCFs are moved
 my $outDir;
 
-(@ARGV == 2) || die "need 2 args: inDir outDir\n";
+(@ARGV == 2) || die "E $0: need 2 args: inDir outDir\n";
 
 ($inDir,$outDir) = @ARGV;
 
 (-d $outDir) || (mkdir($outDir)) || 
-    die "outDir $outDir doesn't exist and can't be mkdir'd\n";
+    die "E $0: outDir $outDir doesn't exist and can't be mkdir'd\n";
 
-opendir(IN, $inDir) || die "cannot opendir inDir $inDir\n";
+opendir(IN, $inDir) || die "E $0: cannot opendir inDir $inDir\n";
 
-while (my $grex = readdir(IN)) {
-    ($grex =~ /^(grexome\d\d\d\d)$/) ||
-	((warn "W: grex dir $grex doesn't look like a grexomedir, skipping\n") && next);
-    my $inFile = "$inDir/$grex/results/variants/genome.S1.vcf.gz";
+while (my $sample = readdir(IN)) {
+    # silently ignore anything starting with '.'
+    ($sample =~ /^\./) && next;
+    my $inFile = "$inDir/$sample/results/variants/genome.S1.vcf.gz";
     foreach my $ext ("", ".tbi") {
 	(-e "$inFile$ext") ||
-	    ((warn "W: skipping $grex$ext because I can't find GVCF$ext file $inFile$ext\n") && next);
-	my $outFile = "$outDir/$grex.g.vcf.gz$ext";
+	    ((warn "W $0: skipping $sample$ext because I can't find GVCF$ext file $inFile$ext\n") && next);
+	my $outFile = "$outDir/$sample.g.vcf.gz$ext";
 	(-e $outFile) &&
-	    die "E: about to move GVCF$ext for $grex but it already exists in outDir $outDir, WTF!!\n";
+	    die "E $0: about to move GVCF$ext for $sample but it already exists in outDir $outDir, WTF!!\n";
 
 	rename("$inFile$ext",$outFile) ||
-	    die "E: rename failed for $inFile $outFile\n";
+	    die "E $0: rename failed for $inFile $outFile\n";
     }
 }
 

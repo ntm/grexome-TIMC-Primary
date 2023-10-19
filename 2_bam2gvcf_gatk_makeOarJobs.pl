f80b347dc7c9cc6922d208d715f5d75214548972
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2023-10-19 21:45:00 +0200

    2_bam2gvcf_gatk_makeOarJobs.pl: update to current codebase

--- 2_bam2gvcf_gatk_makeOarJobs.pl
+++ 2_bam2gvcf_gatk_makeOarJobs.pl
@@ -23,7 +23,7 @@
 # 08/10/2020
 # NTM
 #
-# submit bam2gvcf_gatk.pl jobs on dahu with oar.
+# submit bam2gvcf_gatk.pl jobs on the dahu cluster with OAR.
 # Not sure if this script can be useful outside our organization:
 # it would need adapting for a different batch scheduler and/or
 # cluster, paths and filename patterns are hard-coded (eg samples
@@ -34,16 +34,16 @@
 #
 # Takes 2 args: $first and $last, the first and last grexomes (samples)
 # for this run of bam2gvcf_gatk_makeOarJobs.pl
-# NOTE: not more than 50*$grexomesPerJob at a time, or you get error:
+# NOTE: not more than 50*$samplesPerJob at a time, or you get error:
 # Admission Rule ERROR : [ADMISSION RULE] Error: you cannot have more than 50 jobs waiting in the queue at the same time.
-
 
 use strict;
 use warnings;
 
-# max number of grexomes to process in a single OAR job of 24h, 
-# assume 8h for 24 samples in parallel -> up to 72 should be OK
-my $grexomesPerJob = 72;
+
+# max number of grexomes to process in a single OAR job:
+# trying to run 8 samples in parallel on a 16-core resource in 8h
+my $samplesPerJob = 8;
 
 (@ARGV == 2) || die "E: need two ints as arguments, first and last\n";
 my ($first,$last) = @ARGV;
@@ -51,39 +51,43 @@ my ($first,$last) = @ARGV;
 ($last >= $first) || 
     die "E: must have first <= last, here we have: first $first last $last\n";
 
-(($last - $first) > 50*$grexomesPerJob) &&
-    die "E: cannot queue more than 50 jobs at a time on OAR/dahu\n";
+(($last - $first) > 50*$samplesPerJob) &&
+    die "E: cannot queue more than 50 jobs == ".50 * $samplesPerJob." samples ($samplesPerJob per job) at a time on OAR/dahu\n";
 
 
 #################################
 # hard-coded stuff
 
-# number of parallel jobs on one node
-my $jobs = 24;
+# number of cores/threads (16 on dahu is nice)
+my $jobs = 16;
 
 # hard-coded dirs:
 # log to $logDir
-my $logDir = "/bettik/thierryn/ProcessBams_GATK_2010_dahu_logs/";
+my $logDir = "/home/thierryn/ProcessBams-GATK_Dahu_stdouterr/";
+(-d $logDir) || mkdir($logDir) || 
+    die "E: logDir $logDir doesn't exist and can't be mkdir'd\n";
 # path to bam2gvcf_gatk.pl
-my $bam2gvcf = "~/Bam2gvcf_GATK_PackagedWithBinaries_Centos7/2_bam2gvcf_gatk.pl";
-# path/to/config.pm
-my $config = "~/Bam2gvcf_GATK_PackagedWithBinaries_Centos7/grexomeTIMCprim_config.pm";
+my $bam2gvcf = "/home/thierryn/Bam2gvcf_GATK_Binaries/2_bam2gvcf_gatk.pl";
 # bams are in $inDir
 my $inDir = "/bettik/thierryn/BAMs_All_Selected/";
 # produce gvcfs in $outDir
-my $outDir = "/bettik/thierryn/ProcessBams_GATK_2010_dahu/";
+my $outDir = "/bettik/thierryn/GVCFs_GATK_Raw/";
+# ref genome
+my $genome = "/bettik/thierryn/HumanGenome/hs38DH.fa";
+# gzipped and tabix-indexed BED file with chromosomes 1-22, X, Y, M
+my $chroms = "/bettik/thierryn/HumanGenome/hs38_chroms.bed.gz";
 
 # path/to/latest/gatk : doesn't work, gatk dies with cryptic message
-#my $gatk = "~/Bam2gvcf_GATK_PackagedWithBinaries_Centos7/gatk-latest/gatk";
+#my $gatk = "/home/nthierry/Bam2gvcf_GATK_Binaries/gatk-latest/gatk";
 # -> instead we use a singularity image
 my $gatk = 'singularity exec';
 # bind /bettik/thierryn/ (ie make it rw-accessible from within the container),
 # /home/thierryn/ is bound by default
 $gatk .= ' --bind /bettik/thierryn/';
 # path/to/image
-##$gatk .= ' ~/Bam2gvcf_GATK_PackagedWithBinaries_Centos7/gatk-latest.sif';
+$gatk .= ' /home/thierryn/Bam2gvcf_GATK_Binaries/gatk-latest.sif';
 # running from the sif doesn't work, trying a sandbox
-$gatk .= ' ~/gatk_4.1.8.1_SANDBOX';
+# $gatk .= ' ~/gatk_4.1.8.1_SANDBOX';
 
 # run gatk in bash -c with leading "( so we can capture stderr... $bam2gvcf must
 # close the paren and quote after redirecting stderr
@@ -91,32 +95,33 @@ $gatk .= ' bash -c \" ( gatk';
 
 # oarsub command with params: run on my project ngs-timc, and...
 my $oarBase = "oarsub --project ngs-timc";
-## INITIAL BIG JOB TO PROCESS GREXOMES 50-489: ask for 1 full node, 24h walltime
-##$oarBase .= " -l /nodes=1,walltime=24 ";
+# many samples: ask for 16 cores on 1 node, 8h walltime (should be enough for 8 samples)
+$oarBase = " -l /nodes=1/core=$jobs,walltime=8 ";
 # for a single sample: 4 cores on 1 node for 12h:
-$oarBase .= " -l /nodes=1/core=4,walltime=12 ";
+## $oarBase .= " -l /nodes=1/core=4,walltime=12 ";
 
-my $grex1 = $first;
-my $grex2 = $first + $grexomesPerJob - 1;
-while ($grex1 <= $last) {
-    ($grex2 <= $last) || ($grex2 = $last);
-    # build comma-separated list of samples
-    my $samples = "";
-    foreach my $s ($grex1..$grex2) {
-	# left-pad with zeroes to 4 digits
-	($s < 10) && ($s = "0$s");
-	($s < 100) && ($s = "0$s");
-	($s < 1000) && ($s = "0$s");
-	$samples .= "grexome$s,";
+
+#################################
+# queue jobs
+
+my @samples = ();
+foreach my $gNum ($first..$last) {
+    my $grexome = $gNum;
+    # left-pad with zeroes to 4 digits
+    ($gNum < 10) && ($grexome = "0$grexome");
+    ($gNum < 100) && ($grexome = "0$grexome");
+    ($gNum < 1000) && ($grexome = "0$grexome");
+    $grexome = "grexome$grexome";
+    push(@samples, $grexome);
+    if ((@samples == $samplesPerJob) || ($gNum == $last)) {
+	# choose stdout and stderr filenames
+	my $sampsString = $samples[0]."-".$samples[$#samples];
+	my $oar = $oarBase."-O $logDir/bam2gvcfGatk.$sampsString.out -E $logDir/bam2gvcfGatk.$sampsString.err ";
+	$oar .= "\" perl $bam2gvcf --indir $inDir --genome $genome --chroms $chroms --outdir $outDir --samples ";
+	$oar .= join(',', @samples);
+	$oar .= " --tmpdir /var/tmp/ --jobs $jobs --gatk \'$gatk\' --real\"";
+	warn "$oar\n";
+	#system($oar);
+	@samples = ();
     }
-    # chop trailing comma
-    chop($samples);
-    my $oar = $oarBase." -O $logDir/bam2gvcf.$grex1-$grex2.out -E $logDir/bam2gvcf.$grex1-$grex2.err ";
-    $oar .= "\" perl $bam2gvcf --indir $inDir --samples $samples --outdir $outDir --gatk \'$gatk\' --config $config --jobs $jobs --real\"";
-
-    #warn "$oar\n";
-    system($oar);
-
-    $grex1 = $grex2 + 1;
-    $grex2 = $grex1 + $grexomesPerJob - 1;
 }
